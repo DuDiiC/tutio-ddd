ccd06930f58dabdd2b8953746947096b97a8dc8a
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2022-03-22 14:48:12 +0100

    Add generic event producer for RabbitMQ

---
+++ server-app/BookingService/infrastructure/src/main/java/com/ddd/tutio/booking/rabbitmq/producer/RabbitEventsProducer.java
@@ -0,0 +1,29 @@
+package com.ddd.tutio.booking.rabbitmq.producer;
+
+import com.ddd.tutio.base.DomainEvent;
+import comd.ddd.tutio.booking.port.event.producer.BookingEventsProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitEventsProducer implements BookingEventsProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitEventsProducer.class);
+
+    private final String eventsExchange;
+    private final RabbitTemplate template;
+
+    public RabbitEventsProducer(@Value("${rabbitmq.exchange.events}") String eventsExchange, RabbitTemplate template) {
+        this.eventsExchange = eventsExchange;
+        this.template = template;
+    }
+
+    @Override
+    public void publishEvent(DomainEvent event) {
+        logger.info("Produce event: {}", event.getClass().getCanonicalName());
+        template.convertAndSend(eventsExchange, event.getClass().getCanonicalName(), event);
+    }
+}
