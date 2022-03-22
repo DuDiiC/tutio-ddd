2b48373658c39189299f5da58c21aa6884df0673
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2022-03-22 14:36:14 +0100

    Add generic interface for events producer

---
+++ server-app/BookingService/application/src/main/java/comd/ddd/tutio/booking/port/event/producer/BookingEventsProducer.java
@@ -0,0 +1,17 @@
+package comd.ddd.tutio.booking.port.event.producer;
+
+import com.ddd.tutio.base.DomainEvent;
+
+import java.util.Collection;
+import java.util.Comparator;
+
+public interface BookingEventsProducer {
+
+    void publishEvent(DomainEvent event);
+
+    default void publishEvents(Collection<DomainEvent> events) {
+        events.stream()
+                .sorted(Comparator.comparing(event -> event.createdOn))
+                .forEach(this::publishEvent);
+    }
+}
