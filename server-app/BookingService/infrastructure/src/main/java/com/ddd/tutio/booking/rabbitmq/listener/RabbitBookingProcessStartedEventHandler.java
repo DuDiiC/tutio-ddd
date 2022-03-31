5edcf2152d2fe8bcf7b3c9b51b131a69edb8ff88
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2022-03-31 15:32:53 +0200

    Add implementation of BookingProcessStarted listener

--- server-app/BookingService/infrastructure/src/main/java/com/ddd/tutio/booking/rabbitmq/listener/RabbitBookingProcessStartedEventHandler.java
+++ server-app/BookingService/infrastructure/src/main/java/com/ddd/tutio/booking/rabbitmq/listener/RabbitBookingProcessStartedEventHandler.java
@@ -1,39 +1,46 @@
 package com.ddd.tutio.booking.rabbitmq.listener;
 
-import com.ddd.tutio.base.DomainEvent;
+import com.ddd.tutio.booking.BookingTemplate;
+import com.ddd.tutio.booking.BookingTemplateRepository;
 import com.ddd.tutio.booking.event.BookingProcessStarted;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ddd.tutio.booking.port.event.listener.BookingProcessStartedEventHandler;
+import com.ddd.tutio.booking.port.event.producer.BookingEventsProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
 
 @Component
 public class RabbitBookingProcessStartedEventHandler implements BookingProcessStartedEventHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(RabbitBookingProcessStartedEventHandler.class);
 
-    private final ObjectMapper mapper;
+    private final BookingTemplateRepository bookingTemplateRepository;
+    private final BookingProcessStartedValidator validator;
+    private final BookingEventsProducer eventsProducer;
 
-    public RabbitBookingProcessStartedEventHandler(ObjectMapper mapper) {
-        this.mapper = mapper;
+    public RabbitBookingProcessStartedEventHandler(
+            BookingTemplateRepository bookingTemplateRepository,
+            BookingProcessStartedValidator validator, BookingEventsProducer eventsProducer
+    ) {
+        this.bookingTemplateRepository = bookingTemplateRepository;
+        this.validator = validator;
+        this.eventsProducer = eventsProducer;
     }
 
     @Override
+    @Transactional
     @RabbitListener(queues = "${rabbitmq.queue.booking.booking-process-started}")
     public void handle(BookingProcessStarted event) {
-        // TODO - implement
-        logConsumption(event);
-    }
-
-    private void logConsumption(DomainEvent event) {
-        try {
-            String eventJson = mapper.writeValueAsString(event);
-            logger.info("Consume event: {}\nEvent value: \n{}", event.getClass().getCanonicalName(), eventJson);
-        } catch (JsonProcessingException e) {
-            logger.warn(e.getMessage());
+        if (validator.validate(event)) {
+            BookingTemplate bookingTemplate = new BookingTemplate(bookingTemplateRepository.generateNext(), event);
+            bookingTemplateRepository.add(bookingTemplate);
+            eventsProducer.publishEvents(bookingTemplate.events());
+        } else {
+            throw new IllegalArgumentException();
         }
+        logger.info("Consume event: {}\n ID: {}", event.getClass().getCanonicalName(), event.eventId);
     }
 }
